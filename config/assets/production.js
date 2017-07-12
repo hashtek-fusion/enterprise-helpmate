d97efb157064a259476d9dee80e9f0e8e14fc0b7
Rajesh Ramamoorthy <rrajesh@gmail.com>
2017-07-12 12:48:42 -0500

    Modified the Production configuration files

--- config/assets/production.js
+++ config/assets/production.js
@@ -14,10 +14,15 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
         'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js'
+        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/chart.js/dist/Chart.min.js',
+        'public/lib/angular-chart.js/dist/angular-chart.min.js',
+        'public/lib/angular-spinner/dist/angular-spinner.min.js',
+        'public/lib/angular-local-storage/dist/angular-local-storage.min.js',
+        'public/lib/sugar/dist/sugar.min.js'
       ]
-    },
-    css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js'
+    }
+    //css: 'public/dist/application.min.css',
+    //js: 'public/dist/application.min.js'
   }
 };
--- config/env/production.js
+++ config/env/production.js
@@ -1,13 +1,13 @@
 'use strict';
-
 module.exports = {
-  secure: true,
-  port: process.env.PORT || 8443,
+  //secure: true,
+  port: process.env.PORT || 7000,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/helpmate-dev',
     options: {
-      user: '',
-      pass: ''
+        user: 'detsadmin',
+        pass: 'mongoadmin',
+        auth:{authSource:'admin'}
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
